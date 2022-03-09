a99e65f4b26f58498e84d58eec1d31a91706545b
Michael Kaye <1917473+michaelkaye@users.noreply.github.com>
2022-03-09 11:14:03 +0000

    Initial update to create action structure

---
+++ create.js
@@ -0,0 +1,14 @@
+const core = require('@actions/core');
+
+
+// most @actions toolkit packages have async methods
+async function run() {
+  try {
+    core.info(`Starting synapse ...`);
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
