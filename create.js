d044023cb5b8830ee75b9bdee2e3797961ed85e8
Michael Kaye <1917473+michaelkaye@users.noreply.github.com>
2023-08-21 11:54:08 +0100

    Try forcing poetry==1.5.1 (#96)

--- create.js
+++ create.js
@@ -21,8 +21,8 @@ async function run() {
         process.chdir("synapse");
         await exec.exec("python", ["-m", "pip", "install","pipx"]);
         await exec.exec("python", ["-m", "pipx", "ensurepath"]);
-        await exec.exec("pipx", ["install", "poetry"]);
-        await exec.exec("poetry", ["install", "--extras", "all"]);
+        await exec.exec("pipx", ["install", "poetry==1.5.1"]);
+        await exec.exec("poetry", ["install", "-vv", "--extras", "all"]);
     } 
     else {
         // installing from pypi does not need the checkout.
